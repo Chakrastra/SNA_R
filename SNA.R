8c2210621ca3b5ff8d58ee49bb1fd8a0b987e9ca
chakrastra <rajakvivek080@gmail.com>
2024-06-07 11:46:50 +0530

    Read data and creating network

--- SNA.R
+++ SNA.R
@@ -3,6 +3,8 @@
 install.packages("igraph")
 library(igraph)
 
+
+# working of graphs in R
 g<-graph(c(1,2))
 plot(g) #example of plotting graph
 plot(g,
@@ -18,21 +20,58 @@ plot(g,
      edge.color="red")
 g[] #this give sparse matrix of graph
 
-g1<- graph(c("Amy","Ram","Ram","Li","Li","Amy",
-             "Amy","Li","Kate","Li"))
+g1<- graph(c("Soham","Ram","Ram","Abhi","Abhi","Soham",
+             "Soham","Abhi","Vivek","Abhi"))
 plot(g1,
-     vertex.color="green",
-     edge.color="red")
+     vertex.size = 30,            # Size of the nodes
+     vertex.label.cex = 0.8,      # Size of the label text
+     vertex.label.dist =0,       # Distance of the label from the node
+     vertex.label.color = "blue", # Color of the label
+     edge.arrow.size = 0.5,       # Size of the arrow heads
+     edge.width = 2,              # Width of the edges
+     edge.color = "red",          # Color of the edges
+     layout = layout.auto)        # Automatic layout of the graph
 g1
 
 #Network measures
 degree(g1)
+degree(g1,mode = "all")
+degree(g1,mode = "in")
+degree(g1,mode = "out")
+
+diameter(g1,directed = F,weights = NA)
+
+edge_density(g1,loops = FALSE)
+ecount(g1)/(vcount(g1)*(vcount(g1)-1))
+
+reciprocity(g1)
+closeness(g1,mode = 'all',weights = NA)
+betweenness(g1,directed = T,weights = NA)
+
+edge_betweenness(g1,directed = T,weights = NA)
+
+#Reading Data file
+
+data<- read.csv(file.choose(), header = T)
+View(data)
+
+x<-data.frame(data$first,data$second)
+
+#Creating Network
+
+net<-graph.data.frame(x,directed = T)
+V(net) # vertices in a network
+E(net) # Edges in the network
+
+V(net)$user<-V(net)$name #labels
+V(net)$connections<-degree(net) #degree
+V(net)$connections
+V(net)$user
 
 
 
-
-
-
+## we can use any layouts from the above they all are slightly different 
+#whichever shows good layout of network use that then 
 
 
 
